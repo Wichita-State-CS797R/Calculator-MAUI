6ca9144c3e179d45eb7e5b6d16a048f4d67ede16
Palle3 <palleapoorva23@gmail.com>
2023-11-28 23:59:12 -0600

    Adding MVVM

---
+++ Calculator/CalcHistoryHelper.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+
+namespace Calculator
+{
+    public partial class CalcHistoryHelper : ObservableObject
+    {
+
+        public CalcHistoryHelper()
+        {
+            Calcs = new List<CalcHistoryModel>();
+        }
+
+
+        [ObservableProperty]
+        public static List<CalcHistoryModel> calcs;
+
+
+    }
+
+}
