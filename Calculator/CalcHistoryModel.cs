77b509bdd2281ac00e9a0dd42e232984dc9d4b67
AvinashReddy35 <avinashreddymamidala@gmail.com>
2023-11-28 22:25:19 -0600

    Adding the Calculator History Model.

---
+++ Calculator/CalcHistoryModel.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace Calculator
+{
+    public class CalcHistoryModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string CurrentCalculation { get; set; }
+    }
+
+}   
